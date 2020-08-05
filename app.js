ee155083382d425c1bd9d94503b5f5854db774f2
Raul Davila <rauldavila323@gmail.com>
2020-08-04 21:57:12 -0700

    Worked on understanding .scrollY and innerHeight to find the pixel position of images.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 As long as this debounce() functiom continues to be invoked, it will not be triggered. The function will be called after it stops being called for N milliseconds. If immediate is passed as an argument to the function, the function triggers immediately and then waits for the interval before being called again.
 */
 
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-            
+                    //It will only run the fuction every 20 miliseconds.
+function debounce(func, wait = 10, immediate = true) {
+    var timeout;      
         return function () {
             var context = this, args = arguments;
             
@@ -25,11 +25,32 @@ function debounce(func, wait = 20, immediate = true) {
 }
 
 //This selects all the images that will be seen on the page.
-const sliderImage = document.querySelectorAll('slide-in');
+const sliderImage = document.querySelectorAll('.slide-in');
 
 //This will run everytime someone scrolls
 function checkslide(e){
-    console.log(e);
+    //Founds out how far the page is down.
+   sliderImage.forEach(sliderImage => {
+       //window.scrollY tells you how much has scrolled from the top.
+
+       //window.innerHeight will give you the pixel level at where you are currently in the bottom 
+
+       //sliderImage.height will give us where the bottom pixel of the picture is located and we divide by 2 to get half
+
+       //half way 
+       const slideInAt =(window.scrollY + window.innerHeight) - sliderImage.height / 2;
+
+       //bottom of image through the image
+       const imageBottom = sliderImage.offsetTop + sliderImage.height;
+       const isHalfShown = slideInAt > sliderImage.offsetTop;
+       const isNotScrolledPast = window.scrollY < imageBottom;
+
+       if(isHalfShown && isNotScrolledPast){
+           sliderImage.classList.add('active');
+       }else{
+           sliderImage.classList.remove('active');
+       }
+   })
 }
 
 //when the window is scrolled you will run the function checkslide.
